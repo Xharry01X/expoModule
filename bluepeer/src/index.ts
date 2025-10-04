91e4f31cb933fe68f018cebfef8137f744448e80
Harry 🦀 <130093039+Xharry01X@users.noreply.github.com>
2025-10-04 20:14:42 +0000

    adding events.

--- bluepeer/src/index.ts
+++ bluepeer/src/index.ts
@@ -1,5 +1,20 @@
-import BluepeerModule from "./BluepeerModule"
+import { EventEmitter } from 'expo';
+import BluepeerModule from './BluepeerModule';
+
+const emitter: any = new EventEmitter(BluepeerModule);
+
+export type ThemeChangeEvent = {
+  theme: string;
+};
+
+export function addThemeListener(listener: (event: ThemeChangeEvent) => void): any {
+  return emitter.addListener('onChangeTheme', listener);
+}
 
 export function getTheme(): string {
-    return BluepeerModule.getTheme()
+  return BluepeerModule.getTheme();
+}
+
+export function setTheme(theme: string): void {
+  BluepeerModule.setTheme(theme);
 }
